cd370d43c8ab12ef1205c1760befd554fbc4566e
entcheva <christina.entcheva@gmail.com>
2017-02-02 09:54:02 -0500

    Done.

---
+++ components/Invitation.js
@@ -0,0 +1,19 @@
+const React = require('react')
+const
+
+
+class Invitation extends React.Component {
+
+  render () {
+    return (
+      <div>
+        <h1>You have been invited!</h1>
+      </div>
+    )
+  }
+
+}
+
+
+
+module.exports = Invitation
---
+++ components/ThemedDecorations.js
@@ -0,0 +1,17 @@
+const React = require('react')
+const = ThemedDecorations = require('.ThemedDecorations')
+
+
+class ThemedDecorations extends React.Component {
+
+
+
+
+
+
+
+
+}
+
+
+module.exports = ThemedDecorations
